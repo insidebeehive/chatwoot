96977bb213665b91f403e38ce33c736b1da8905e
Sagar Chavda <126857566+sagarcakewalk@users.noreply.github.com>
2025-06-20 13:34:10 +0400

    change: update auto resolve min duration from 10 to 2 minutes

--- app/javascript/dashboard/routes/dashboard/settings/account/components/AutoResolve.vue
+++ app/javascript/dashboard/routes/dashboard/settings/account/components/AutoResolve.vue
@@ -93,7 +93,7 @@ const updateAccountSettings = async settings => {
 };
 
 const handleSubmit = async () => {
-  if (duration.value < 10) {
+  if (duration.value < 2) {
     useAlert(t('GENERAL_SETTINGS.FORM.AUTO_RESOLVE.DURATION.ERROR'));
     return Promise.resolve();
   }
