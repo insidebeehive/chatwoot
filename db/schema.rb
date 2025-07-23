14c282a340e2bd1a75b264c87034e86bcdc79346
Sagar Chavda <126857566+sagarcakewalk@users.noreply.github.com>
2025-07-23 12:43:36 +0530

    fix: voice notes not loading correctly
    # Conflicts:
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -937,9 +937,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_14_104358) do
     t.string "homepage_link"
     t.string "page_title"
     t.text "header_text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.jsonb "config", default: {"allowed_locales" => ["en"]}
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.jsonb "config", default: {"allowed_locales"=>["en"]}
     t.boolean "archived", default: false
     t.bigint "channel_web_widget_id"
     t.index ["channel_web_widget_id"], name: "index_portals_on_channel_web_widget_id"
